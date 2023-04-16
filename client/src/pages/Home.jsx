788190a4fc9df51cc4524e5d4513df38266139b6
Andrew Wallace <wall0419@algonquinlive.com>
2023-04-16 16:54:48 -0400

    added new logos

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
             {snap.intro && (
                 <motion.section className='home' {...slideAnimation('left')}>
                     <motion.header {...slideAnimation('down')}>
-                        <img src='./threejs.png' alt='logo' className='w-8 h-8 object-contain'/>
+                        <img src='./xsang_logo.png' alt='logo' className='w-8 h-8 object-contain'/>
                     </motion.header>
 
                     <motion.div className='home-content' {...headContainerAnimation}>
--- client/src/store/index.jsx
+++ client/src/store/index.jsx
@@ -5,7 +5,7 @@ const state = proxy({
     color: '#EFBD48',
     isLogoTexture: true,
     isFullTexture: false,
-    logoDecal: './threejs.png',
+    logoDecal: './xsang_logo.png',
     fullDecal: './threejs.png',
 });
 
